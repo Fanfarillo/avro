f0d8e0d5b356e6121ebd90bafab56665edbf6499
Fanfarillo <matteo.fanfarillo99@gmail.com>
2022-05-21 20:53:01 +0200

    Little fix in dummy test

--- lang/java/tools/src/test/java/DummyTest.java
+++ lang/java/tools/src/test/java/DummyTest.java
@@ -1,17 +1,18 @@
 import org.junit.Test;
-import junit.framework.TestCase;
 
 import org.apache.avro.tool.IdlTool;
 
+import static junit.framework.TestCase.assertEquals;
+
 public class DummyTest {
-	
+
 	@Test
 	public void getNameTest() {
-		
+
 		IdlTool idlTool = new IdlTool();
 		assertEquals("idl", idlTool.getName());
 		assertEquals("Generates a JSON schema from an Avro IDL file", idlTool.getShortDescription());
-		
+
 	}
-	
+
 }
