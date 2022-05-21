623dc370f263d77658f32603ea88a16cdc0969a0
Fanfarillo <matteo.fanfarillo99@gmail.com>
2022-05-21 21:28:15 +0200

    Another retry on dummy test...

--- lang/java/tools/src/test/java/org/apache/avro/tool/DummyTest.java
+++ lang/java/tools/src/test/java/org/apache/avro/tool/DummyTest.java
@@ -22,7 +22,9 @@ import static org.junit.Assert.fail;
 import org.junit.Test;
 
 public class DummyTest {
-  /** Make sure that tool descriptions fit in 80 characters. */
+  /**
+   * Make sure that tool descriptions fit in 80 characters.
+   */
   @Test
   public void testToolDescriptionLength() {
     Main m = new Main();
@@ -30,24 +32,6 @@ public class DummyTest {
       // System.out.println(t.getName() + ": " + t.getShortDescription().length());
       if (m.maxLen + 2 + t.getShortDescription().length() > 80) {
         fail("Tool description too long: " + t.getName());
-      }
-    }
-  }
-
-  /**
-   * Make sure that the tool name is not too long, otherwise space for description
-   * is too short because they are rebalanced in the CLI.
-   */
-  @Test
-  public void testToolNameLength() {
-    // 13 chosen for backwards compatibility
-    final int MAX_NAME_LENGTH = 13;
-
-    Main m = new Main();
-    for (Tool t : m.tools.values()) {
-      if (t.getName().length() > MAX_NAME_LENGTH) {
-        fail("Tool name too long (" + t.getName().length() + "): " + t.getName() + ". Max length is: "
-          + MAX_NAME_LENGTH);
       }
     }
   }
