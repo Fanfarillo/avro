58cdbb90bd1365195effff34b580aeb6a898010e
Fanfarillo <matteo.fanfarillo99@gmail.com>
2022-06-11 12:19:13 +0200

    Tried to improve ITs...

--- lang/java/avro/src/test/java/org/apache/avro/reflect/ReflectDataSchemaIT.java
+++ lang/java/avro/src/test/java/org/apache/avro/reflect/ReflectDataSchemaIT.java
@@ -12,8 +12,6 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.hamcrest.Matchers.isA;
-import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
 
 @RunWith(Parameterized.class)
@@ -41,23 +39,67 @@ public class ReflectDataSchemaIT {
       case EMPTY:
         this.names = new HashMap<>();
         break;
-      case VALID:
+      case VAL_VAL:
         this.names = new HashMap<>();
         Schema schema1 = Schema.create(Schema.Type.STRING);
         Schema schema2 = Schema.create(Schema.Type.NULL);
         this.names.put("Schema1", schema1);
         this.names.put("Schema2", schema2);
         break;
-      case INVALID:
+      case VAL_INV:
         this.names = new HashMap<>();
         Schema schema = Schema.createEnum(null, null, null, null);
-        this.names.put(null, schema);
+        this.names.put("Schema", schema);
         break;
 
-      case MOCKED:
+      case VAL_MOCK:
         this.names = new HashMap<>();
         Schema mockedSchema = getMockedSchema();
         this.names.put("Mocked_Schema", mockedSchema);
+        break;
+
+      case VAL_NULL:
+        this.names = new HashMap<>();
+        this.names.put("No_Schema", null);
+        break;
+      case EMP_VAL:
+        this.names = new HashMap<>();
+        Schema valSchema = Schema.create(Schema.Type.STRING);
+        this.names.put("", valSchema);
+        break;
+      case EMP_INV:
+        this.names = new HashMap<>();
+        Schema invSchema = Schema.createEnum(null, null, null, null);
+        this.names.put("", invSchema);
+        break;
+      case EMP_MOCK:
+        this.names = new HashMap<>();
+        Schema mockedSchema2 = getMockedSchema();
+        this.names.put("", mockedSchema2);
+        break;
+      case EMP_NULL:
+        this.names = new HashMap<>();
+        this.names.put("", null);
+        break;
+      case NULL_VAL:
+        this.names = new HashMap<>();
+        Schema valSchema2 = Schema.create(Schema.Type.STRING);
+        this.names.put(null, valSchema2);
+        break;
+      case NULL_INV:
+        this.names = new HashMap<>();
+        Schema invSchema2 = Schema.createEnum(null, null, null, null);
+        this.names.put(null, invSchema2);
+        break;
+      case NULL_MOCK:
+        this.names = new HashMap<>();
+        Schema mockedSchema3 = getMockedSchema();
+        this.names.put(null, mockedSchema3);
+        break;
+      case NULL_NULL:
+        this.names = new HashMap<>();
+        this.names.put(null, null);
+        break;
       }
 
       switch (typeType) {
@@ -71,17 +113,19 @@ public class ReflectDataSchemaIT {
         this.expectedOutput = Schema.create(Schema.Type.STRING);
         break;
       case INVALID:
-        ParamType[] arrayPT = {};
-        this.type = arrayPT.getClass().getComponentType();
+        Schema[] arraySch = null;
+        this.type = arraySch.getClass().getComponentType();
         this.expectedOutput = null;
         break;
       }
 
     } catch (NullPointerException e1) {
-      Assert.assertSame(
-          "NullPointerException should be thrown during configuration only if namesType == INVALID." + " Instead, "
-              + e1.getClass().getName() + " has been thrown and namesType == " + namesType + ".",
-          namesType, ParamType.INVALID);
+      Assert.assertTrue(
+          "NullPointerException should be thrown during configuration only if Type is invalid or"
+              + "Schema is invalid. Instead, " + e1.getClass().getName() + " has been thrown and namesType == "
+              + namesType + ".",
+          namesType == ParamType.VAL_INV || namesType == ParamType.EMP_INV || namesType == ParamType.NULL_INV
+              || typeType == ParamType.INVALID);
       this.isExceptionThrown = true;
 
     } catch (Exception e2) {
@@ -95,16 +139,18 @@ public class ReflectDataSchemaIT {
   public static Collection<Object[]> getParameters() {
     return Arrays.asList(new Object[][] {
         // TYPE NAMES EXCEPTION
-        { ParamType.VALID, ParamType.MOCKED, false }, { ParamType.NULL, ParamType.NULL, true },
-        { ParamType.VALID, ParamType.EMPTY, false }, { ParamType.VALID, ParamType.VALID, false },
-        { ParamType.INVALID, ParamType.INVALID, true }, });
+        { ParamType.NULL, ParamType.NULL, true }, { ParamType.NULL, ParamType.EMP_MOCK, true },
+        { ParamType.NULL, ParamType.EMP_NULL, true }, { ParamType.INVALID, ParamType.EMP_VAL, true },
+        { ParamType.INVALID, ParamType.EMP_INV, true }, { ParamType.VALID, ParamType.EMPTY, false },
+        { ParamType.VALID, ParamType.VAL_VAL, false }, { ParamType.VALID, ParamType.VAL_INV, true },
+        { ParamType.VALID, ParamType.VAL_MOCK, false }, { ParamType.VALID, ParamType.VAL_NULL, false },
+        { ParamType.VALID, ParamType.NULL_VAL, false }, { ParamType.VALID, ParamType.NULL_INV, true },
+        { ParamType.VALID, ParamType.NULL_MOCK, false }, { ParamType.VALID, ParamType.NULL_NULL, false }, });
   }
 
   private Schema getMockedSchema() {
 
-    Schema mockedSchema = mock(Schema.class);
-    doNothing().when(mockedSchema).equals(isA(Object.class));
-    return mockedSchema;
+    return mock(Schema.class);
 
   }
 
@@ -131,7 +177,8 @@ public class ReflectDataSchemaIT {
   }
 
   private enum ParamType {
-    NULL, EMPTY, VALID, INVALID, MOCKED
+    NULL, EMPTY, VALID, INVALID, VAL_VAL, VAL_INV, VAL_MOCK, VAL_NULL, EMP_VAL, EMP_INV, EMP_MOCK, EMP_NULL, NULL_VAL,
+    NULL_INV, NULL_MOCK, NULL_NULL
   }
 
 }
